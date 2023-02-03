4e8afe2733813cb2effa0dda76688b1bcd93a2e6
VISHNU V <vishnusathyanathan@gmail.com>
2023-02-03 23:51:55 +0530

    made some changes

---
+++ config/cloudinary.js
@@ -0,0 +1,29 @@
+import cloudinary from "cloudinary"
+import CloudinaryStorage from "multer"
+
+//const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+
+})
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "chat-app",
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+})
+
+export default {cloudinary,storage}
+
+
+/*
+module.exports = {
+    cloudinary,
+    storage
+}
+*/
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
 import express from "express"
 import { login, userSignUp } from "../controllers/userControllers.js";
+import multer from "multer";
+import storage from "../config/cloudinary.js"
+
+
+
 
 const userRouter = express.Router();
+const upload = multer({storage : storage})
 
-userRouter.post("/signup",userSignUp);
+
+
+userRouter.post("/signup",upload.single("imageFile"),userSignUp)
 userRouter.post("/",login)
 
 export default userRouter;
+
